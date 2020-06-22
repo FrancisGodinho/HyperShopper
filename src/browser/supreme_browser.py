4da2f09be3c36fbfdf3efa685b3fc019ef902829
francisgodinho <francisgodinho2010@hotmail.com>
2020-06-21 21:19:06 -0700

    removed some unneeded files

--- src/browser/supreme_browser.py
+++ src/browser/supreme_browser.py
@@ -4,20 +4,45 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 
 from browser.browser import Browser
+from user.user import User
 
 class SupremeBrowser(Browser):
     
-    def add_to_cart(self, sub_cat, item_id):
+    def add_to_cart(self, sub_cat, item_id, timeout=10):
         self.driver.get(f"https://www.supremenewyork.com/shop/{sub_cat}/{item_id}")
-        self.driver.find_element_by_name("commit").click()
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cart"]/a[2]'))).click()
+        WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, ' //*[@id="add-remove-buttons"]/input'))).click()
+        WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cart"]/a[2]'))).click()
     
-    def add_to_cart_bylink(self, link):
+    def add_to_cart_bylink(self, link, timeout=10):
         self.driver.get(link)
-        self.driver.find_element_by_name("commit").click()
-        WebDriverWait(self.driver, 20).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cart"]/a[2]'))).click()
+
+        user_size = self.user.shirt_size()
+        if "pants" in link or "shorts" in link:
+            user_size = self.user.pant_size()
+        if "shoes" in link:
+            user_size = self.user.shoe_size()
+
+        # Check if this item has sizes and select the appropriate size 
+        try:
+            if self.driver.find_element_by_xpath('//*[@id="s"]').get_attribute("aria-labelledby") is not None:
+                if self.user.shirt_size().upper() not in WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="s"]'))).get_attribute('innerHTML').upper():
+                    return False
+
+                WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="s"]'))).send_keys(user_size)
+        except NoSuchElementException:
+            pass
+        
+        # Add item to cart if possible
+        try:
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, ' //*[@id="add-remove-buttons"]/input'))).click()
+            WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cart"]/a[2]'))).click()
+        except TimeoutException:
+            return False
+
+        return True
 
     def checkout(self, timeout=10):
         WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="order_billing_name"]'))).send_keys(self.user.get_data("name"))
@@ -35,3 +60,4 @@ class SupremeBrowser(Browser):
 
         WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="cart-cc"]/fieldset/p[2]/label/div'))).click()
         WebDriverWait(self.driver, timeout).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="pay"]/input'))).click()
+
